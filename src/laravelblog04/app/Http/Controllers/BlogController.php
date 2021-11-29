e1be79acfdb67300c7ae2a99499bb4d54e9bd6ad
iam-akari <haradaakari3845@gmail.com>
2021-11-29 17:25:42 +0900

    更新、削除機能追加

--- src/laravelblog04/app/Http/Controllers/BlogController.php
+++ src/laravelblog04/app/Http/Controllers/BlogController.php
@@ -69,5 +69,64 @@ class BlogController extends Controller
         return redirect(route('blogs'));
     } 
 
+    /**
+     * 編集画面表示
+     * @return view
+     * @param int $id
+     */
+    public function showEdit($id)
+    {
+        $blog = Blog::find($id);
+
+        if(is_null($blog)){
+          \Session::flash('err_msg','データがありません。');
+          return redirect(route('blogs'));
+        }
+        return view('blog.edit',['blog'=>$blog]);
+    }
+
+    public function exeUpdate(BlogRequest $request)
+    {
+       $inputs = $request->all();
+
+       \DB::beginTransaction();
+       try {
+          $blog = $request->find($inputs['id']);
+          $blog = fill([
+              'title' => $title,
+              'content' => $content
+          ]);
+          $blog->save();
+          \DB::commit();
+       } catch (\Throwable $th) {
+           \DB::rollback();
+           abort(500);
+       }
+       
+       \Session::flash('err_msg','更新しました。');
+       return redirect(route('blogs'));
+    }
+
+    /**
+     * 削除
+     * @return view
+     * @param int $id
+     */
+    public function exeDelete($id)
+    {
+        if(empty($id)){
+            \Session::flash('err_msg','データがありません。');
+            return redirect(route('blogs'));
+        }
+
+        try {
+            Blog::destroy($id);
+        } catch (\Throwable $e) {
+            about(500);
+        }
+      
+      \Session::flash('err_msg','削除しました。');  
+      return redirect(route('blogs'));
+    }
 
 }
